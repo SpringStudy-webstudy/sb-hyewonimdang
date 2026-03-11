7cd80edb97a1455a5c4fbb2a59e8708446470c16
hyewonimdang <hyehyekuk202@gmail.com>
2026-03-12 02:41:25 +0900

    chore: init project setup

---
+++ src/test/java/com/springStudy/sb_hyewonimdang/SbHyewonimdangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springStudy.sb_hyewonimdang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbHyewonimdangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
